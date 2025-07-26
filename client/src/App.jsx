470c9324af6799d501d3b612ddc6157306cb545b
anigawade45 <[anigawade05@gmail.com]>
2025-07-26 15:54:02 +0530

    backend folder setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 
-const App = () => { 
+const App = () => {
   return (
     <>
       <Navbar />
